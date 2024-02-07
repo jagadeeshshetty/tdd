daffe48af5f9daf93d35640c9149ef3e658e388e
Jagadeesh C <jagadeesh.cheluvaraj@gmail.com>
2024-02-08 01:32:39 +0530

    added PlusOrMinus program.

---
+++ src/main/java/com/jc/aim/algo/while_loop/PlusOrMinus.java
@@ -0,0 +1,22 @@
+package com.jc.aim.algo.while_loop;
+
+import java.util.Scanner;
+
+public class PlusOrMinus {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int testCaseCount = sc.nextInt();
+    while (testCaseCount > 0) {
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int c = sc.nextInt();
+
+      if (a + b == c)
+        System.out.println('+');
+      else
+        System.out.println('-');
+
+      testCaseCount--;
+    }
+  }
+}
