94bdf3850fb5fe6163d26bd4a3e4d58b629264cb
jagadeesh-wsl <jagadeesh.cheluvaraj@gmail.com>
2024-02-14 17:38:55 +0530

    done: PrintAllSubArray

--- src/main/java/com/jc/aim/algo/arrays/PrintAllSubArray.java
+++ src/main/java/com/jc/aim/algo/arrays/PrintAllSubArray.java
@@ -1,26 +1,35 @@
 package com.jc.aim.algo.arrays;
 
+import java.util.Scanner;
+
 public class PrintAllSubArray {
   public static void main(String[] args) {
-    int N = 5;
-    int[] arr = { 1, 3, 1, 5, 8 };
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    int[] arr = new int[N];
+    for (int i = 0; i < N; i++) {
+      arr[i] = sc.nextInt();
+    }
+
+    if (N <= 0) {
+      System.out.println(-1);
+      return;
+    }
+
     int totalSubArrays = 0;
 
+    // int[] arr = { 1, 3, 1, 5, 8 };
+
     // start, end index approach
-    for (int start = 0; start < N; start++) {
-      for (int end = start; end < N; end++) {
-        printArray(arr, start, end);
-        System.out.print(", ");
+    for (int st = 0; st < N; st++) {
+      for (int en = st; en < N; en++) {
+        for (int i = st; i <= en; i++) {
+          System.out.print(arr[i] + " ");
+        }
+        System.out.println();
         totalSubArrays++;
       }
-      System.out.println();
     }
     System.out.println("Total Subarrays: " + totalSubArrays);
-  }
-
-  static void printArray(int[] arr, int start, int end) {
-    for (int i = start; i <= end; i++) {
-      System.out.print(arr[i] + " ");
-    }
   }
 }
