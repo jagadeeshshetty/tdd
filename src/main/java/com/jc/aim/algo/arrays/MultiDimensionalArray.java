637349020ff80d70705100795642110d536e57b4
jagadeesh-wsl <jagadeesh.cheluvaraj@gmail.com>
2024-02-24 00:18:00 +0530

    update

--- src/main/java/com/jc/aim/algo/arrays/MultiDimensionalArray.java
+++ src/main/java/com/jc/aim/algo/arrays/MultiDimensionalArray.java
@@ -4,20 +4,31 @@ import java.util.Arrays;
 
 public class MultiDimensionalArray {
   public static void main(String[] args) {
+    // 2x3 array
     int[][] arr = {
-        { 1, 2, 3, 4 },
-        { 5, 6, 7, 8 },
-        { 9, 10, 11, 12 }
+        { 1, 2, 3 },
+        { 4, 5, 6 }
     };
-    System.out.println(arr);
-    System.out.println(Arrays.toString(arr));
+
     for (int i = 0; i < arr.length; i++) {
       System.out.println(Arrays.toString(arr[i]));
     }
 
-    int[][] arrDiff = { new int[2], new int[4], new int[3] };
-    for (int i = 0; i < arrDiff.length; i++) {
-      System.out.println(Arrays.toString(arrDiff[i]));
+    // row traverse using forEach loop
+    for (int row = 0; row < arr.length; row++) {
+      for (int val : arr[row]) {
+        System.out.print(val + " ");
+      }
+      System.out.println();
     }
+
+    // row traverse using for loop
+    for (int row = 0; row < arr.length; row++) {
+      for (int col = 0; col < arr[row].length; col++) {
+        System.out.print(arr[row][col] + " ");
+      }
+      System.out.println();
+    }
+
   }
 }
