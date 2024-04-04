4961cea79b96515840f5c7ec59b9b54d8454fe57
jagadeesh-wsl <jagadeesh.cheluvaraj@gmail.com>
2024-04-04 22:40:55 +0530

    update

---
+++ src/main/java/com/jc/aim/algo/array_list/Test.java
@@ -0,0 +1,19 @@
+package com.jc.aim.algo.array_list;
+
+import java.util.ArrayList;
+
+public class Test {
+
+  public static void main(String[] args) {
+
+    // create ArrayList
+    ArrayList<String> languages = new ArrayList<>();
+
+    // Add elements to ArrayList
+    languages.add("Java");
+    languages.add("JavaScript");
+    languages.add("Python");
+    System.out.println("ArrayList: " + languages);
+  }
+
+}
---
+++ src/main/java/com/jc/aim/algo/arrays/multi_dimensional/MultiplyMatrices.java
@@ -0,0 +1,29 @@
+package com.jc.aim.algo.arrays.multi_dimensional;
+
+public class MultiplyMatrices {
+  public static void main(String[] args) {
+    // Scanner sc = new Scanner(System.in);
+    // int N = sc.nextInt();
+    // int[][] arr = new int[N][N];
+    // for (int i = 0; i < N; i++) {
+    // for (int j = 0; j < N; j++) {
+    // arr[i][j] = sc.nextInt();
+    // }
+    // }
+
+    int[][] arr1 = {
+        { 1, 2, 3 },
+        { 4, 5, 6 }
+    };
+
+    int[][] arr2 = {
+        { 10, 11 },
+        { 20, 21 },
+        { 30, 31 }
+    };
+
+    int[][] arr3 = {};
+
+    
+  }
+}
